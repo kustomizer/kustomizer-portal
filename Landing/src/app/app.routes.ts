79ee60431055597f57211250094146dbb11935d3
GonzaNicolari <gonzalo.nicolari@gmail.com>
2025-12-23 19:07:18 -0300

    basic connection to supabase and list users

--- Landing/src/app/app.routes.ts
+++ Landing/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'users',
+    component: UserListComponent
+  },
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full'
+  }
+];
---
+++ Landing/src/environment/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://hficxeuuutbutwadvtze.supabase.co',
+  supabaseKey: 'sb_publishable_pk7TBiO6Atg10YHaURQkcg_kr0oJ0VS'
+};
