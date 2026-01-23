7afdcfcbe3d4264c02004d825017243b157472b7
Saikus08 <agustinvignolosotelo@gmail.com>
2026-01-22 23:11:53 -0300

    refactor: add nvmrc and fix render modes

--- Landing/src/app/app.routes.server.ts
+++ Landing/src/app/app.routes.server.ts
@@ -3,34 +3,34 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
   },
   {
     path: 'pricing',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
   },
   {
     path: 'how-it-works',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
   },
   {
     path: 'contact',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
   },
   {
     path: 'login',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
   },
   {
     path: 'app',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
   },
   {
     path: 'admin',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
   },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
--- Landing/src/environment/environment.development.ts
+++ Landing/src/environment/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://riajjyijwtsnxomhddam.supabase.co',
-  supabaseKey: 'sb_publishable_H66YFm41qOfm9EZgFLLp1w_bJn8YjBm'
+  supabaseKey: 'sb_publishable_H66YFm41qOfm9EZgFLLp1w_bJn8YjBm',
 };
-
